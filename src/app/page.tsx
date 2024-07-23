ce94095f10312a3a0b4723d127a117af5f1efab3
arkingco <arkingco@gmail.com>
2024-07-23 17:54:17 +0900

    feat: swr 라이브러리 추가 #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
+"use client";
+
 import * as Popover from "@radix-ui/react-popover";
+import useSWR from "swr";
+
 export default function Home() {
+  const fetcher = () => fetch("/api/test").then((res) => res.json());
+  const { data, error, isLoading } = useSWR("/api/test", fetcher);
+
+  console.log(data);
+  if (error) {
+    console.log("error");
+    return;
+  }
+  if (isLoading) {
+    console.log("loading");
+    return;
+  }
   return (
-    <Popover.Root>
-      <Popover.Trigger className="mx-10 mt-10">
-        성준님 열어보세요
-      </Popover.Trigger>
-      <Popover.Portal>
-        <Popover.Content className="">
-          ㅎㅇ
-          <Popover.Arrow className="" />
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+    <div>
+      <Popover.Root>
+        <Popover.Trigger className="mx-10 mt-10">
+          성준님 열어보세요
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content className="">
+            {data.text}
+            <Popover.Arrow className="" />
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
+    </div>
   );
 }
