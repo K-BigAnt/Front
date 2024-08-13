503c5bdf62c1aaf5014642248949cd608eacb6c0
arkingco <arkingco@gmail.com>
2024-08-13 11:45:16 +0900

    feat: 백테스트 페이지 api 요청 로직 추가 #5

--- src/app/backtest/page.tsx
+++ src/app/backtest/page.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
+import axios from "axios";
+import useSWR from "swr";
 
 const Financial = dynamic(() => import("../component/Chart/Financial"), {
   ssr: false,
@@ -12,6 +14,16 @@ const Pie = dynamic(() => import("../component/Chart/Pie"), {
 });
 
 export default function BacktestPage() {
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:80/v1/stock/price?symbol=000050&start_date=2024-04-01&end_date=2024-07-01",
+    fetcher
+  );
+
+  console.log(data);
+  if (error) return <div>Error: {error.message}</div>;
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className="flex flex-col items-center justify-center mt-10">
       {typeof window !== "undefined" && <Pie />}
