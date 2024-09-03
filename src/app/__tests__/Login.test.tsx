f8b005a20cb4a43ed58e0acedd7b711125a4da82
lamPolar <lovast7@gmail.com>
2024-09-03 19:05:38 +0900

    test : login컴포넌트 test 추가-logo render #15

--- src/app/__tests__/Login.test.tsx
+++ src/app/__tests__/Login.test.tsx
@@ -7,7 +7,13 @@ describe("Login Component", () => {
   beforeEach(() => {
     render(<Login />);
   });
-        
+  
+  it("login logo 테스트", () => {
+    const logo = screen.getByRole("img", { name: /logo/i }); // 로고의 alt 텍스트에 맞게 수정
+    expect(logo).toBeInTheDocument();
+    expect(logo.tagName).toBe("svg"); // 로고가 SVG 형식인지 확인
+  });
+
   it("login title 테스트", () => {
     expect(screen.getByText(/BigAnt와 함께/i)).toBeInTheDocument();
     expect(screen.getByText(/1억 모으기/i)).toBeInTheDocument();
