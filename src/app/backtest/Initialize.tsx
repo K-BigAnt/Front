9322fa275b20259745c7bc5a86913d764a8c2b6a
arkingco <arkingco@gmail.com>
2024-09-10 18:52:31 +0900

    feat: 종목 추가 버튼 생성 및 데이터 구조 통일화 #12

--- src/app/backtest/Initialize.tsx
+++ src/app/backtest/Initialize.tsx
@@ -4,13 +4,38 @@ import Dropdown from "../component/common/Dropdown";
 import { useInput } from "../hooks/useInput";
 import StockSearch from "./StockSearch";
 
+interface stockInfo {
+  startDate: string;
+  endDate: string;
+  initialAsset: number;
+  rebalancing: string;
+  stock: string[];
+}
+
 export default function Initialize() {
   const [initAmount, , initOnChange] = useInput(
     "",
     (value: string) => value.length < 10
   );
-
   const [rebalancing, setRebalancing] = useState("");
+
+  const [stock, setStock] = useState<stockInfo>({
+    startDate: "",
+    endDate: "",
+    initialAsset: 0,
+    rebalancing: "반기별",
+    stock: [],
+  });
+
+  const handleAddStock = () => {
+    setStock({ ...stock, stock: [...stock.stock, ""] });
+  };
+
+  const handleBacktest = () => {
+    // 백테스트 실행하기 버튼시 stock 정보를 백엔드로 보내기
+    console.log(stock);
+  };
+
   return (
     <div>
       <div className="flex flex-row items-center justify-center">
@@ -23,6 +48,10 @@ export default function Initialize() {
                 <input
                   type="month"
                   className="border-2 border-gray-300 rounded-md"
+                  value={stock.startDate}
+                  onChange={(e) =>
+                    setStock({ ...stock, startDate: e.target.value })
+                  }
                 />
               </div>
               <div>
@@ -30,6 +59,10 @@ export default function Initialize() {
                 <input
                   type="month"
                   className="border-2 border-gray-300 rounded-md"
+                  value={stock.endDate}
+                  onChange={(e) =>
+                    setStock({ ...stock, endDate: e.target.value })
+                  }
                 />
               </div>
             </div>
@@ -49,13 +82,26 @@ export default function Initialize() {
             initialValue="리밸런싱"
             seleted={rebalancing}
             setSelected={setRebalancing}
-            options={["월별", "분기별", "반기별", "연별"]}
+            options={["반기별"]}
           />
         </div>
         <div className="flex flex-col items-center justify-center gap-y-10">
           포트폴리오
-          <StockSearch />
+          {stock.stock.map((item, index) => {
+            return (
+              <StockSearch
+                key={item + index}
+                stock={stock}
+                setStock={setStock}
+              />
+            );
+          })}
         </div>
+
+        <button onClick={handleAddStock}>종목 추가</button>
+        <button className="ml-10" onClick={handleBacktest}>
+          백테스트 실행
+        </button>
       </div>
     </div>
   );
