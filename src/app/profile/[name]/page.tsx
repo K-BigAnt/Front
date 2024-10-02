c413b4f2b0fb12b228985d60cb54c0eb5ea7181f
arkingco <arkingco@gmail.com>
2024-10-02 19:58:01 +0900

    feat: 검색창 추가 #22

--- src/app/profile/[name]/page.tsx
+++ src/app/profile/[name]/page.tsx
@@ -14,11 +14,12 @@ export default function Profile({ params }: {params: {name: string}}) {
   }, []);
 
   return (
-    <div>
-      <div className="flex flex-row items-center justify-center h-[200px]">
-        <div className="w-[25%] h-[200px]  flex items-center justify-center">
-          <div className="w-[150px] h-[150px] rounded-full bg-red-300"></div>
-        </div>
+    <div className="flex flex-row items-center">
+      <div className="w-[80%]">
+        <div className="flex flex-row items-center justify-center h-[200px]">
+          <div className="w-[25%] h-[200px]  flex items-center justify-center">
+            <div className="w-[150px] h-[150px] rounded-full bg-red-300"></div>
+          </div>
         <div className="w-[55%] h-[200px]flex flex-col items-start justify-center pl-2">
           <div className="text-[48px] font-medium">{params.name}</div>
           <div className="text-[24px] font-medium">프로필 소개</div>
@@ -28,6 +29,10 @@ export default function Profile({ params }: {params: {name: string}}) {
           <Button variant="outline" className="w-[100px]">언팔로우</Button>
           <Button variant="outline" className="w-[100px]">프로필 수정</Button>
         </div>
+      </div>
+      </div>
+      <div className="w-[20%]">
+        검색창
       </div>
     </div>
   )
